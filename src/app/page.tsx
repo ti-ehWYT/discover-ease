880caf9a440e1e1cf095aefcde2cf6ab08cef507
ti-ehWYT <tawanyen3@gmail.com>
2025-07-02 07:34:14 +0800

    Update layout styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,14 @@ import PostDialog from "@/components/post-dialog";
 
 export default async function Home() {
   const { data } = await getPosts();
-  
+
   return (
-    <div className="columns-1 sm:columns-2 md:columns-3 gap-4 p-4">
+    <div className="columns-1 sm:columns-2 md:columns-3 gap-4">
       {data.map((item) => (
-        <PostDialog key={item.id} postItem={item} allowEdit={false} />
+        <div key={item.id} className="mb-4 break-inside-avoid">
+           <PostDialog   postItem={item} allowEdit={false} />
+          </div>
+       
       ))}
     </div>
   );
