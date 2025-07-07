2a114365641a97c0e0adce302df6b770cf5d66bc
ti-ehWYT <tawanyen5@gmail.com>
2025-07-07 22:55:44 +0800

    edit back post scroll and added hongkong

--- src/components/post-dialog.tsx
+++ src/components/post-dialog.tsx
@@ -1,6 +1,4 @@
-"use client";
-
-import { PencilIcon, ChevronLeft, ChevronRight } from "lucide-react";
+import { PencilIcon } from "lucide-react";
 import { Post } from "../../type/post";
 import { Card, CardContent, CardHeader } from "./ui/card";
 import {
@@ -13,7 +11,7 @@ import {
 } from "./ui/dialog";
 import Link from "next/link";
 import Image from "next/image";
-import { useRef, useState } from "react";
+import { Carousel, CarouselContent, CarouselItem } from "./ui/carousel";
 
 export default function PostDialog({
   postItem,
@@ -22,30 +20,6 @@ export default function PostDialog({
   postItem: Post;
   allowEdit: boolean;
 }) {
-  const carouselRef = useRef<HTMLDivElement>(null);
-  const [currentIndex, setCurrentIndex] = useState(0);
-  const totalImages = postItem.images?.length || 0;
-
-  const scrollToIndex = (index: number) => {
-    if (carouselRef.current) {
-      carouselRef.current.scrollTo({
-        left: index * carouselRef.current.clientWidth,
-        behavior: "smooth",
-      });
-      setCurrentIndex(index);
-    }
-  };
-
-  const scrollLeft = () => {
-    const prevIndex = currentIndex === 0 ? totalImages - 1 : currentIndex - 1;
-    scrollToIndex(prevIndex);
-  };
-
-  const scrollRight = () => {
-    const nextIndex = currentIndex === totalImages - 1 ? 0 : currentIndex + 1;
-    scrollToIndex(nextIndex);
-  };
-
   return (
     <Dialog key={postItem.id}>
       <DialogTrigger asChild>
@@ -76,46 +50,27 @@ export default function PostDialog({
         className=" flex max-h-3/4 max-w overflow-hidden"
         style={{ minWidth: "60vw" }}
       >
-        <div className="flex-1 relative">
-          <div
-            ref={carouselRef}
-            className="flex overflow-x-auto space-x-4 snap-x snap-mandatory scroll-smooth"
-          >
-            {postItem.images?.map((image, index) => (
-              <div
-                key={image}
-                className="snap-start relative min-w-full h-[70vh] rounded-md"
-              >
-                <Image
-                  src={`https://firebasestorage.googleapis.com/v0/b/discover-ease-ee29d.firebasestorage.app/o/${encodeURIComponent(
-                    image
-                  )}?alt=media`}
-                  alt={`Image ${index + 1}`}
-                  fill
-                  className="object-cover rounded-md"
-                />
-              </div>
-            ))}
-          </div>
-
-          {postItem.images && postItem.images.length > 1 && (
-            <>
-              <button
-                onClick={scrollLeft}
-                className="absolute top-1/2 left-2 transform -translate-y-1/2 bg-white/80 p-2 rounded-full z-10 hover:bg-white"
-              >
-                <ChevronLeft />
-              </button>
-              <button
-                onClick={scrollRight}
-                className="absolute top-1/2 right-2 transform -translate-y-1/2 bg-white/80 p-2 rounded-full z-10 hover:bg-white"
-              >
-                <ChevronRight />
-              </button>
-            </>
-          )}
-
+        <div className="flex-1">
+          <Carousel>
+            <CarouselContent>
+              {postItem.images?.map((image, index) => (
+                <CarouselItem key={image}>
+                  <div className="rounded-md relative h-[70vh] min-h-80">
+                    <Image
+                      src={`https://firebasestorage.googleapis.com/v0/b/discover-ease-ee29d.firebasestorage.app/o/${encodeURIComponent(
+                        image
+                      )}?alt=media`}
+                      alt={`Image ${index + 1}`}
+                      fill
+                      className="object-cover rounded-md"
+                    />
+                  </div>
+                </CarouselItem>
+              ))}
+            </CarouselContent>
+          </Carousel>
         </div>
+
         <div className="flex-1 ml-4 flex flex-col overflow-hidden">
           <DialogTitle className="flex justify-between">
             {postItem.title}
